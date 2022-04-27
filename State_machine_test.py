64406c9d40db8e525a354b47f349c356a5f117fc
PimVanHeugten <vanheugtenpim@gmail.com>
2022-04-27 14:37:45 +0200

    state_machine edit

--- State_machine_test.py
+++ State_machine_test.py
@@ -1,12 +1,5 @@
 import smach
 
-# class MachineState(StateMachine):
-#     Go_Block = State('go to block')
-#     Claim_Block = State('pick block')
-#     Avoid_Collision = State('pick block')
-#     Go_Dropzone = State('pick block')
-#     Drop_Block = State('pick block')
-
 class GO_BLOCK(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['error1', 'outcome1'])
@@ -27,7 +20,6 @@ class CLAIM(smach.State):
             return 'outcome2'
         return 'error2'
             
-
 class GO_ZONE(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['error3', 'outcome3'])
@@ -38,7 +30,6 @@ class GO_ZONE(smach.State):
             return 'outcome3'
         return 'error3'
             
-
 class DROP(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['error4', 'outcome4'])
@@ -55,7 +46,7 @@ class COLLISION(smach.State):
     def execute(self, userdata):
         while dist < 150:
             # Your state execution goes here
-            
+            x=37
         if x==1:
             return 'out1'
         elif x==2:
@@ -66,7 +57,7 @@ class COLLISION(smach.State):
             return 'out4'
 
 while True:
-    sm = smach.StateMachine(outcomes=['outcome4','outcome5'])
+    sm = smach.StateMachine( """outcomes=['outcome4','outcome5']""" )
     with sm:
         smach.StateMachine.add('GO_BLOCK', GO_BLOCK(), transitions={'error1':'COLLISION','outcome1':'CLAIM'})
         smach.StateMachine.add('CLAIM', CLAIM(), transitions={'error2':'COLLISION','outcome2':'GO_ZONE'})
