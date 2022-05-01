dd1be45046432b374dcd68185f6929169b844a4b
Karel Van Hoey <karel.vanhoey@ugent.be>
2022-05-01 11:36:59 +0200

    Move functions to functions_karel.py and add grab_image()

--- Stack_Maker.py
+++
@@ -1,7 +0,0 @@
-def create_stack(list_angles, list_distances):
-    stack = []
-    for i in range(len(list_angles)):
-        stack.append(['rot', list_angles[i]])
-        stack.append(['transl', list_distances[i]])
-    
-    return stack
