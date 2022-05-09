eed37044ca5e5d1e0072259b9959fff8edb818f3
RobinCeu <robinceuppens@me.com>
2022-05-09 14:35:56 +0200

    problem solving path finding

--- Driving_commands.py
+++ Driving_commands.py
@@ -1,12 +1,11 @@
 import numpy as np
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 import cv2
+import pygame
+import time
 from Aruco_Detection import our_position_heading
+from RRT_Game import RRTGraph
+from RRT_Game import RRTMap
+from playing_field import init, recognition
 
 from Aruco_Detection import positioning, findAruco, our_position_heading
 #import Aruco_Detection
@@ -20,10 +19,6 @@ from Aruco_Detection import positioning, findAruco, our_position_heading
 #Then, calculate distance and use move command
 #Remark possibly need to adjust ange according to aruco after reaching every consecutive node?
 
-ev3 = EV3Brick()
-LM = Motor(Port.A, Direction.CLOCKWISE) #left motor
-RM = Motor(Port.D, Direction.CLOCKWISE) #right motor
-FM = Motor(Port.B, Direction.CLOCKWISE) #Front motor
 
 meters_per_pixles = 0.00526834
 D_wheel = 0.05
@@ -36,9 +31,10 @@ class RRT_Drive:
         self.number_of_nodes = len(X)
         print("initialized with: ", self.number_of_nodes)
 
-    def get_angle(self, img):
-        cv2.imshow('img', img)
+    def get_angle(self, img, direction_facing):
+        cv2.imshow('test', img)
         th = []
+        print(self.number_of_nodes)
         for i in range(0,self.number_of_nodes-1):
             if self.X[i+1]-self.X[i] == 0:
                 if self.Y[i+1]-self.Y[i] > 0:
@@ -48,10 +44,8 @@ class RRT_Drive:
             else:
                 th.append(round(np.arctan((self.Y[i+1]-self.Y[i])/(self.X[i+1]-self.X[i]))*180/np.pi,1))
     
-        print(our_position_heading(img)[1])
-        comm = [our_position_heading(img)[1]]
+        comm = [direction_facing]
         print("comm: ", comm)
-        #comm = ["starting angle"]
         for i in range(1,len(th)):
             comm.append(round(th[i]-th[i-1],1))
         return comm
@@ -62,22 +56,111 @@ class RRT_Drive:
             dis.append(round(np.sqrt((self.X[i+1]-self.X[i])**2+(self.Y[i+1]-self.Y[i])**2),1))
         return dis
 
-    def execute_movements(self,Xs,Ys,n_movements,turn_rate=100,forward_speed=1080):
+    
+def load_instructions(cap):
+        dimensions =(512,512)
+        Xs, Ys = ([],[])
+        start=(50,50)
+        goal = (300,300)
+        obsdim=30 
+        _, img = cap.read()
+        _, pts, goal, _, _ = init(cap)
+        _, blue, green, red = recognition(cap,pts)
+        obstacle_coords = []
+        for e in [blue,green,red]:
+            for i in range(len(e)):
+                obstacle_coords.append(list(e[i]))
+        print("obstacles: ",obstacle_coords)
+        graph=RRTGraph(start,goal,dimensions,obsdim,obstacle_coords)
+        print(start,goal,dimensions,obsdim,obstacle_coords)
+        l = len(graph.getPathCoords())
+        print("l : ", l)
+        for e in graph.getPathCoords():
+            Xs.append(e[0])
+            Ys.append(e[1])
+        Xs.reverse()
+        Ys.reverse()
+        print("\n")
+        print(l)
+        print("Xs: ", Xs)
+        print("Ys: ", Ys)
         instructions = RRT_Drive(Xs,Ys)
-        th = instructions.get_angle()
-        dis = instructions.get_distance()
-        meters_per_pixles = 0.00526834
-        D_wheel = 0.05
-        Track_width = 0.125 
-        for i in range(n_movements):
-            #Turn in right direction
-            degrees_to_turn = Track_width/D_wheel*th[i]
+        print("angles: ", instructions.get_angle(img))
+        print("distances: ", instructions.get_distance())
+        print("\n")
+        angles = instructions.get_angle(img)
+        distances = instructions.get_distance()
+        return angles, distances
 
-            LM.on_for_degrees(turn_rate, th[i])
-            RM.on_for_degrees(turn_rate, -th[i])
+def load_instructions_bis(cap, aruco_friend, direction_facing):
 
-            #Move towards next node
-            degrees_to_turn = dis[i]*meters_per_pixles/(D_wheel*np.pi)*360
-            LM.on_for_degrees(forward_speed,degrees_to_turn)
-            RM.on_for_degrees(forward_speed,degrees_to_turn)
+    dimensions =(512,512)
+    start = aruco_friend
+    obsdim=30
+    obstacle_coords = []
+    _, img = cap.read()
+    cv2.imshow('image', img)
+    warped, pts, goal, goal_centre, field = init(cap)
 
+    goal = (300,300)
+    _, blue, green, red = recognition(cap,pts)
+    for e in [blue,green,red]:
+        for i in range(len(e)):
+            obstacle_coords.append(list(e[i]))
+    iteration=0
+    t1=0
+
+    pygame.init()
+    map=RRTMap(start,goal,dimensions,obsdim,obstacle_coords)
+    graph=RRTGraph(start,goal,dimensions,obsdim,obstacle_coords)
+
+    obstacles=graph.makeobs()
+    map.drawMap(obstacles)
+
+    t1=time.time()
+    while (not graph.path_to_goal()):
+        elapsed=time.time()-t1
+        t1=time.time()
+        #raise exception if timeout
+        if elapsed > 10:
+            print('Kon geen pad maken')
+            raise
+
+        if iteration % 10 == 0:
+            X, Y, Parent = graph.bias(goal)
+            pygame.draw.circle(map.map, map.grey, (X[-1], Y[-1]), map.nodeRad*2, 0)
+            pygame.draw.line(map.map, map.Blue, (X[-1], Y[-1]), (X[Parent[-1]], Y[Parent[-1]]),
+                             map.edgeThickness)
+
+        else:
+            X, Y, Parent = graph.expand()
+            pygame.draw.circle(map.map, map.grey, (X[-1], Y[-1]), map.nodeRad*2, 0)
+            pygame.draw.line(map.map, map.Blue, (X[-1], Y[-1]), (X[Parent[-1]], Y[Parent[-1]]),
+                             map.edgeThickness)
+
+        if iteration % 5 == 0:
+            pygame.display.update()
+        iteration += 1
+
+    map.drawPath(graph.getPathCoords())
+
+    Xs, Ys = ([], [])
+    l = len(graph.getPathCoords())
+    for e in graph.getPathCoords():
+        Xs.append(e[0])
+        Ys.append(e[1])
+    Xs.reverse()
+    Ys.reverse()
+    instructions = RRT_Drive(Xs,Ys)
+    angles = instructions.get_angle(img, direction_facing)
+    distances = instructions.get_distance()
+    print("\n")
+    print("angles: ", angles)
+    print("distances: ", distances)
+    print("\n")
+
+    print(pts)
+    pygame.display.update()
+    pygame.event.clear()
+    pygame.event.wait(500)
+    return angles, distances
---
+++ dommetest.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+from Driving_commands import load_instructions_bis
+from functions_vic import *
+from functions_karel import *
+from Aruco_Detection import *
+import pygame
+
+#IP_adress = '192.168.1.15'
+#cap = cv2.VideoCapture('http://'+IP_adress+':8000/stream.mjpg')
+# cap = cv2.VideoCapture(0)
+# # _, img = cap.read()
+
+cap = cv2.VideoCapture('http://192.168.1.15:8000/stream.mjpg')
+_, img = cap.read()
+HSV_blue = np.array([[74, 112, 43], [179, 255, 255]])
+HSV_red = np.array([[0, 114, 68], [75, 255, 255]])
+HSV_green = np.array([[28, 67, 94], [128, 255, 255]])
+
+print("Here 1")
+warped, pts, goal, goal_centre, field = init(cap)
+print("Here 2")
+
+
+aruco_friend = []
+while aruco_friend == []:
+    aruco_friend, direction_facing = our_position_heading(img)
+starting_position = tuple(aruco_friend)
+direction_facing = direction_facing[0]
+
+friendly_goal, enemy_goal, enemy_goal_centre = goal_allocation(aruco_friend, goal, goal_centre)
+
+warped, blue_in, green_in, red_in, blue_out, green_out, red_out = recognition(cap, pts, enemy_goal, HSV_blue,HSV_red,HSV_green)
+print("ee")
+load_instructions_bis(cap, aruco_friend, direction_facing)
+pygame.event.wait(0)
+
+# while True:
+#     #_, img = cap.read()
+#     img = cv2.imread("aruco_transformed.png")  # make sure path is correct and terminal is in right folder
+
+#     # _, _, _, ids, img, corners = findAruco(img)
+    
+#     cv2.imshow('img', img)
+#     if cv2.waitKey(1) == 113:       # Q-key as quit button
+#         break
