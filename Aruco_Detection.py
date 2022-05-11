193147c81cf3edacc002262e9f22d56e23e0d6fa
PimVanHeugten <vanheugtenpim@gmail.com>
2022-05-11 12:27:14 +0200

    edit Aruco detection

--- Aruco_Detection.py
+++ Aruco_Detection.py
@@ -2,10 +2,10 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 
-# IP_adress = '192.168.1.15'
-# cap = cv2.VideoCapture('http://'+IP_adress+':8000/stream.mjpg')
-# _, img = cap.read()
-img = cv2.imread("aruco_transformed_2.png")
+IP_adress = '192.168.1.15'
+cap = cv2.VideoCapture('http://'+IP_adress+':8000/stream.mjpg')
+_, img = cap.read()
+#img = cv2.imread("aruco_transformed_2.png")
 
 def findAruco(img, draw=False):
     arucoDict = aruco.Dictionary_get(getattr(aruco, 'DICT_5X5_50'))
@@ -87,16 +87,20 @@ def our_position_heading(img):
 
     return our_position, our_heading
 
-def their_position_heading(img):
+def their_position_heading(img, x=0.0):
     cX, cY, heading, ids, img, _ = findAruco(img)
     _, _, _, their_position, their_heading = positioning(cX, cY, heading, ids)
+
+    if len(their_heading) != 0:
+        their_heading[0] += x
 
     return their_position, their_heading
 
 def enemyOrientation(img):                                                      #tested with aruco_transformed_2.png
-    clone = img.copy()                                                          #used to redraw arrows in enemyOrientation(img)
-    their_position, their_heading = their_position_heading(img)
     x = 0
+    their_position, their_heading = their_position_heading(img)
+    clone1 = img.copy()                                                          #used to redraw arrows in enemyOrientation(img)
+    clone2 = img.copy() 
     while True:
         R = [100*np.cos(their_heading[0]), -100*np.sin(their_heading[0])]       #rotation amount
         A = [round(num) for num in their_position[0]]                           #start position arrow
@@ -104,35 +108,45 @@ def enemyOrientation(img):                                                      
         for i in range(2):
             B[i] = A[i] + round(R[i])   	                                    #round result
         
-        #draw and write on image
-        cv2.imshow('img', img)
-        cv2.putText(img, 'rotated by ' + str(x) + ' degrees', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3) 
-        cv2.arrowedLine(img, tuple(A), tuple(B), (255, 255, 255), 3) 
-        #Locations have to be of type int!
+        # draw and write on image
+        cv2.imshow('img', clone1)
+        cv2.putText(clone1, 'rotated by ' + str(x) + ' degrees', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3) 
+        cv2.arrowedLine(clone1, tuple(A), tuple(B), (255, 255, 255), 3) 
+        # Locations have to be of type int!
 
-        #wait on keys
+        # wait on keys
         Key = cv2.waitKey(1)
         if Key == 114:         # r-key
             their_heading[0] += np.pi/4
             x += 45
             x = np.mod(x,360)
-            img = clone.copy()
+            clone1 = clone2.copy()
         elif Key == 82:        # Capital R
             their_heading[0] += np.pi/180
             x += 1 
             x = np.mod(x,360)
-            img = clone.copy()
+            clone1 = clone2.copy()
         elif Key == 116:        # t-key
             their_heading[0] -= np.pi/4
             x -= 45
             x = np.mod(x,360)
-            img = clone.copy()
+            clone1 = clone2.copy()
         elif Key == 84:        # capital T
             their_heading[0] -= np.pi/180
             x -= 1 
             x = np.mod(x,360)
-            img = clone.copy()
+            clone1 = clone2.copy()
         elif Key == 113:       # q-key as quit button
             break
-    # x *= np.pi/180           # uncheck if wanted in radians
-    return x                   # returns # of degrees rotated counterclockwise (in the positive direction)
+
+    if x >= 0 and x <= 180:
+        x = x
+    elif x > 180 and x < 360:
+        x -= 360
+    x *= np.pi/180             # uncheck if wanted in radians
+    return x                   # returns # of radians rotated counterclockwise (in the positive direction)
+
+x = enemyOrientation(img)
+print(x)
+their_position, their_heading = their_position_heading(img)
+print(their_position, their_heading)
